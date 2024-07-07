43466ae0e2554a851a5cc9b1e5e6625269df3983
Your Name <you@example.com>
2024-07-07 22:02:25 +0600

    Rasel's All work

---
+++ Attribute ^=value & attribute $=value/js/custom2.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+// $('.one').css({
+//     'background-color': 'green',
+//     'color': 'white',
+//     'padding': '20px',
+//     'border': '2px solid black'
+// });
+
+$('button').click(function() {
+    $('.one').css('width','+=200px');
+});
+ 
+
+
+
+});
